9a01e8ac83e7c58699f836e97e927b46c2ac7f95
Sheriff O <87389779+29Sheriff@users.noreply.github.com>
2025-12-24 03:31:11 -0700

    updating infoway
    updated infoway to fix error

---
+++ server.js
@@ -0,0 +1,40 @@
+// server.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') filePath = './dsupport.html';
+    
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+    
+    switch (extname) {
+        case '.js': contentType = 'text/javascript'; break;
+        case '.css': contentType = 'text/css'; break;
+        case '.json': contentType = 'application/json'; break;
+        case '.db': contentType = 'application/octet-stream'; break;
+    }
+    
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if(error.code == 'ENOENT') {
+                res.writeHead(404);
+                res.end('File not found');
+            } else {
+                res.writeHead(500);
+                res.end('Server error: ' + error.code);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const port = 8000;
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Access your app at: http://localhost:${port}/dsupport.html`);
+});
